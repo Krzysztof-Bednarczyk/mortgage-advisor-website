70a62a0d1edaaea2c3a6c64fed84ccdd06380a15
Krzysztof Bednarczyk <krisu876@gmail.com>
2026-02-24 23:15:30 +0100

    Initial start: React components and CSS configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Services from './components/Services'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Hero />
+      <Services />
+    </div>
+  )
+}
+
+export default App
